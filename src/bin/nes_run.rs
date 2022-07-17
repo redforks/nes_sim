0977228d72ae30822084947637b5cbefbf42b28f
forks <redforks@gmail.com>
2022-07-17 18:07:02 +0800

    nes_run add quiet mode

--- src/bin/nes_run.rs
+++ src/bin/nes_run.rs
@@ -3,29 +3,34 @@ use nes_sim::{Cpu, Plugin, Instruction, MemoryBank, RamBank};
 use std::io::Read;
 use generic_array::GenericArray;
 use generic_array::typenum;
+use std::env;
 
 fn main() {
+    let args = env::args().collect::<Vec<_>>();
+    let quiet = args.len() >= 2 && args[1] == "--quiet";
+
     let mut f = File::open("6502_functional_test.bin").unwrap();
     let mut ram = RamBank::<typenum::U65536>(GenericArray::default());
     assert_eq!(65536, f.read(&mut ram.0).unwrap());
     let mut cpu = Cpu::new_with_memory_banks(
-        vec![MemoryBank::new(0, 0, Box::new(ram)) ]
+        vec![MemoryBank::new(0, 0, Box::new(ram))]
     );
-    let mut sync_cycle = ReportPlugin::new();
+    let mut sync_cycle = ReportPlugin::new(quiet);
     while !sync_cycle.should_exit() {
         cpu.clock_tick(&mut sync_cycle);
     }
 }
 
 struct ReportPlugin {
+    verbose: bool,
     count: u32,
     last_pc: Option<u16>,
     should_exit: bool,
 }
 
 impl ReportPlugin {
-    fn new() -> ReportPlugin {
-        ReportPlugin { count: 0, last_pc: None, should_exit: false }
+    fn new(quiet: bool) -> ReportPlugin {
+        ReportPlugin { verbose: !quiet, count: 0, last_pc: None, should_exit: false }
     }
 
     fn should_exit(&self) -> bool {
@@ -36,16 +41,22 @@ impl ReportPlugin {
 impl Plugin for ReportPlugin {
     fn start(&mut self, cpu: &Cpu) {
         self.count += 1;
-        println!("[{}] pc: ${:04x}", self.count, cpu.pc);
+
+        if self.verbose {
+            println!("[{}] pc: ${:04x}", self.count, cpu.pc);
+        }
     }
 
     fn end(&mut self, cpu: &Cpu, inst: Instruction) {
-        println!(
-            "{:?}\na: ${:02x}, x: ${:02x}, y: ${:02x}, sp: ${:02x}, p: ${:02x} {} top: ${:02x}\n",
-            inst, cpu.a, cpu.x, cpu.y, cpu.sp, cpu.status, format_flags(cpu), cpu.peek_stack());
+        if self.verbose {
+            println!(
+                "{:?}\na: ${:02x}, x: ${:02x}, y: ${:02x}, sp: ${:02x}, p: ${:02x} {} top: ${:02x}\n",
+                inst, cpu.a, cpu.x, cpu.y, cpu.sp, cpu.status, format_flags(cpu), cpu.peek_stack());
+        }
 
         if let Some(last) = self.last_pc {
             if last == cpu.pc {
+                self.should_exit = true;
                 if cpu.flag(nes_sim::DecimalModeFlag) {
                     // decimal mode not implemented, it is okay to exit test on decimal error,
                     // decimal test is the last of opCode test.
