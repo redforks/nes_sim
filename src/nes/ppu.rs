aa06ff35d0d91c813cf0daf36e2bd87bd46bc80c
forks <redforks@gmail.com>
2023-01-12 16:13:31 +0800

    Implement Ppu

--- src/nes/ppu.rs
+++ src/nes/ppu.rs
@@ -1,5 +1,7 @@
+use crate::mcu::Mcu;
 use crate::to_from_u8;
 use modular_bitfield::prelude::*;
+use std::cell::Cell;
 
 #[derive(Copy, Clone)]
 #[bitfield]
@@ -39,3 +41,78 @@ pub struct PpuStatus {
     __: B5,
 }
 to_from_u8!(PpuStatus);
+
+pub trait PpuDriver {
+    fn set_ctrl(&mut self, ctrl: PpuCtrl);
+    fn set_mask(&mut self, mask: PpuMask);
+    fn get_status(&self) -> PpuStatus;
+    fn get_oma_address(&self) -> u8;
+    fn set_oma_address(&mut self, address: u8);
+    fn get_oma_data(&self) -> u8;
+    fn set_oma_data(&mut self, data: u8);
+    fn set_scroll_position(&mut self, x: u8, y: u8);
+    fn set_address(&mut self, address: u16); // ppu address
+    fn get_data(&self) -> u8;
+    fn set_data(&mut self, data: u8);
+    fn set_dma(&mut self, page_addr: u8);
+}
+
+pub struct Ppu<D: PpuDriver> {
+    driver: D,
+    scroll_x: Cell<Option<u8>>, // set to None after read status register.
+    data_addr_high: Cell<Option<u8>>, // set to None after read status register.
+}
+
+impl<D: PpuDriver> Ppu<D> {
+    pub fn new(driver: D) -> Self {
+        Self {
+            driver,
+            scroll_x: Cell::new(None),
+            data_addr_high: Cell::new(None),
+        }
+    }
+}
+
+impl<D: PpuDriver> Mcu for Ppu<D> {
+    fn read(&self, address: u16) -> u8 {
+        match address {
+            0x2002 => {
+                let status = self.driver.get_status();
+                self.scroll_x.set(None);
+                self.data_addr_high.set(None);
+                status.into()
+            }
+            0x2004 => self.driver.get_oma_data(),
+            0x2007 => self.driver.get_data(),
+            _ => panic!("Can not read from Ppu at address {}", address),
+        }
+    }
+
+    fn write(&mut self, address: u16, value: u8) {
+        match address {
+            0x2000 => self.driver.set_ctrl(value.into()),
+            0x2001 => self.driver.set_mask(value.into()),
+            0x2003 => self.driver.set_oma_address(value),
+            0x2004 => self.driver.set_oma_data(value),
+            0x2005 => {
+                if let Some(x) = self.scroll_x.get() {
+                    self.driver.set_scroll_position(x, value);
+                    self.scroll_x.set(None);
+                } else {
+                    self.scroll_x.set(Some(value));
+                }
+            }
+            0x2006 => {
+                if let Some(high) = self.data_addr_high.get() {
+                    self.driver.set_address((high as u16) << 8 | value as u16);
+                    self.data_addr_high.set(None);
+                } else {
+                    self.data_addr_high.set(Some(value));
+                }
+            }
+            0x2007 => self.driver.set_data(value),
+            0x4014 => self.driver.set_dma(value),
+            _ => panic!("Can not write to Ppu at address {}", address),
+        }
+    }
+}
