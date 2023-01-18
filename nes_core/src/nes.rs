b1f1c37b4d4e34e231b2f3ca36fd99b164030211
forks <redforks@gmail.com>
2023-01-18 11:27:46 +0800

    LowerRam for nes for better performance

--- nes_core/src/nes.rs
+++ nes_core/src/nes.rs
@@ -1,9 +1,10 @@
 use crate::ines::INesFile;
-use crate::mcu::{AddrRemap, MappingMcu, Mcu, Mirror, RamMcu, Region};
+use crate::mcu::{MappingMcu, Mcu, RamMcu, Region};
 use crate::nes::ppu::PpuDriver;
 
 pub mod apu;
 pub mod controller;
+mod lower_ram;
 mod mapper;
 pub mod ppu;
 
@@ -11,31 +12,16 @@ pub fn create_mcu<PD>(file: &INesFile, pd: PD) -> impl Mcu
 where
     PD: PpuDriver + 'static,
 {
-    let low_ram = RamMcu::new([0; 0x2000]);
     let after_ppu = RamMcu::start_from(0x4000, [0; 0x20]);
     let devices = [
-        Region::with_defined(low_ram),
+        Region::with_defined(lower_ram::LowerRam::new()),
         Region::with_defined(ppu::new(pd)),
         Region::with_defined(after_ppu),
     ]
     .into_iter();
     let devices = devices.chain(mapper::create_cartridge(file));
 
-    let mcu = MappingMcu::new(devices.collect());
-    setup_mem_mirror(mcu)
-}
-
-fn setup_mem_mirror<M: Mcu>(m: M) -> impl Mcu {
-    Mirror::new(addr_remaps().collect(), m)
-}
-
-fn addr_remaps() -> impl Iterator<Item = AddrRemap> {
-    [
-        AddrRemap::new(0x0800, 0x0fff, 0x0000),
-        AddrRemap::new(0x1000, 0x17ff, 0x0000),
-        AddrRemap::new(0x1800, 0x1fff, 0x0000),
-    ]
-    .into_iter()
+    MappingMcu::new(devices.collect())
 }
 
 #[macro_use]
---
+++ nes_core/src/nes/lower_ram.rs
@@ -0,0 +1,49 @@
+use crate::mcu::{DefinedRegion, Mcu};
+
+/// Lower nes RAM (0x0000-0x07FF) and remap to 0x0800-0x1FFF
+pub struct LowerRam {
+    ram: [u8; 0xf00],
+}
+
+impl LowerRam {
+    pub fn new() -> LowerRam {
+        LowerRam { ram: [0; 0xf00] }
+    }
+}
+
+impl DefinedRegion for LowerRam {
+    fn region(&self) -> (u16, u16) {
+        (0, 0x1fff)
+    }
+}
+
+impl Mcu for LowerRam {
+    fn read(&self, address: u16) -> u8 {
+        self.ram[address as usize & 0x7ff]
+    }
+
+    fn write(&mut self, address: u16, value: u8) {
+        self.ram[address as usize & 0x7ff] = value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut mcu = LowerRam { ram: [0; 0xf00] };
+        mcu.write(0x0000, 0x12);
+        assert_eq!(mcu.read(0x0000), 0x12);
+        mcu.write(0x07ff, 0x13);
+        assert_eq!(mcu.read(0x07ff), 0x13);
+
+        assert_eq!(mcu.read(0x0800), 0x12);
+        assert_eq!(mcu.read(0x0fff), 0x13);
+        assert_eq!(mcu.read(0x1000), 0x12);
+        assert_eq!(mcu.read(0x17ff), 0x13);
+        assert_eq!(mcu.read(0x1800), 0x12);
+        assert_eq!(mcu.read(0x1fff), 0x13);
+    }
+}
