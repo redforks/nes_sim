d34902d36bcc0acd9e4791de3ba3fe0f2c804409
forks <redforks@gmail.com>
2023-01-21 15:36:27 +0800

    AttributeTable

--- nes_core/src/nes/ppu/name_table.rs
+++ nes_core/src/nes/ppu/name_table.rs
@@ -1,7 +1,31 @@
 use crate::mcu::Mcu;
 use crate::nes::ppu::{Pattern, Tile, TILES_PER_COL, TILES_PER_ROW};
 
+#[derive(Copy, Clone)]
 pub struct AttributeTable<'a>(&'a [u8]);
+
+impl<'a> AttributeTable<'a> {
+    fn new(data: &'a [u8]) -> Self {
+        debug_assert_eq!(data.len(), 0x40);
+        AttributeTable(data)
+    }
+
+    fn palette_idx(&self, tile_x: u8, tile_y: u8) -> u8 {
+        debug_assert!(tile_x < TILES_PER_ROW);
+        debug_assert!(tile_y < TILES_PER_COL);
+        let x = tile_x / 4;
+        let y = tile_y / 4;
+        let idx = (y * 8 + x) as usize;
+        let byte = self.0[idx];
+        match (tile_x % 2, tile_y % 2) {
+            (0, 0) => byte & 0b11,
+            (1, 0) => (byte >> 2) & 0b11,
+            (0, 1) => (byte >> 4) & 0b11,
+            (1, 1) => (byte >> 6) & 0b11,
+            _ => unreachable!(),
+        }
+    }
+}
 
 #[derive(Copy, Clone)]
 pub struct NameTable<'a>(&'a [u8]);
@@ -205,5 +229,33 @@ mod tests {
         assert_eq!(2, name_tables.tile_idx(31, 0));
         assert_eq!(3, name_tables.tile_idx(0, 1));
         assert_eq!(4, name_tables.tile_idx(31, 29));
+    }
+
+    #[test]
+    fn attribute_table() {
+        let mut arr = [0; 64];
+        arr[0] = 0b00_01_10_11;
+        arr[7] = 0b01_10_11_00;
+        arr[8] = 0b10_11_00_01;
+        arr[63] = 0b11_00_01_10;
+        let tbl = AttributeTable(&arr);
+
+        assert_eq!(0b11, tbl.palette_idx(0, 0));
+        assert_eq!(0b10, tbl.palette_idx(1, 0));
+        assert_eq!(0b01, tbl.palette_idx(0, 1));
+        assert_eq!(0b00, tbl.palette_idx(1, 1));
+
+        // palette are same for 2-2 tiles
+        assert_eq!(0b11, tbl.palette_idx(2, 0));
+        assert_eq!(0b11, tbl.palette_idx(0, 2));
+        assert_eq!(0b11, tbl.palette_idx(2, 2));
+
+        assert_eq!(0b00, tbl.palette_idx(30, 0));
+        assert_eq!(0b11, tbl.palette_idx(31, 0));
+        assert_eq!(0b10, tbl.palette_idx(30, 1));
+        assert_eq!(0b01, tbl.palette_idx(31, 1));
+
+        assert_eq!(0b01, tbl.palette_idx(0, 4));
+        assert_eq!(0b11, tbl.palette_idx(31, 29));
     }
 }
