a14e111203539ed8ab9ffceb7cd69a47cf01d115
forks <redforks@gmail.com>
2023-01-21 10:11:25 +0800

    Update web to run rom

--- nes_core/src/nes/machine.rs
+++ nes_core/src/nes/machine.rs
@@ -1,6 +1,7 @@
 use crate::ines::INesFile;
 use crate::nes::create_mcu;
 use crate::{Cpu, EmptyPlugin};
+use image::RgbImage;
 
 pub struct Machine {
     cpu: Cpu,
@@ -15,18 +16,24 @@ impl Machine {
 
     /// Run the machine for a single frame.
     /// `ms`: milliseconds elapsed since last frame.
-    pub fn process_frame(&mut self, ms: f64) {
+    pub fn process_frame(&mut self, ms: f64) -> &RgbImage {
         const CYCLES_PER_MS: u32 = 1789;
         const V_BLANK_CYCLES: u32 = 2273;
         let cycles = (ms * CYCLES_PER_MS as f64) as u32;
         if cycles > V_BLANK_CYCLES {
             // TODO: start ppu vblank, trigger nmi if ppu.should_nmi()
+            let ppu = self.cpu.mcu().get_ppu();
+            ppu.set_v_blank(true);
+            if ppu.should_nmi() {
+                self.cpu.nmi();
+            }
             self.run_ticks(V_BLANK_CYCLES);
             // TODO: end ppu vblank
             self.run_ticks(cycles - V_BLANK_CYCLES);
         } else {
             self.run_ticks(cycles);
         }
+        self.cpu.mcu().get_ppu().render()
     }
 
     fn run_ticks(&mut self, ticks: u32) {
--- nes_web/src/lib.rs
+++ nes_web/src/lib.rs
@@ -14,19 +14,28 @@ mod drivers;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub struct Machine(NesMachine);
+pub struct Machine {
+    inner: NesMachine,
+    ctx: CanvasRenderingContext2d,
+}
 
 #[wasm_bindgen]
-pub fn new_machine(ines: Vec<u8>) -> Machine {
+pub fn new_machine(canvas_id: &str, ines: Vec<u8>) -> Machine {
     debug!("new_machine");
     let ines = INesFile::new(ines).unwrap();
-    Machine(NesMachine::new(ines))
+    Machine {
+        inner: NesMachine::new(ines),
+        ctx: get_canvas_context(canvas_id),
+    }
 }
 
 #[wasm_bindgen]
 impl Machine {
     pub fn process_frame(&mut self, ms: f64) {
-        self.0.process_frame(ms);
+        let img = self.inner.process_frame(ms);
+        let img_data =
+            ImageData::new_with_u8_clamped_array_and_sh(Clamped(&img), 256, 240).unwrap();
+        self.ctx.put_image_data(&img_data, 0.0, 0.0).unwrap();
     }
 }
 
@@ -36,25 +45,29 @@ fn read_chr(f: &INesFile) -> Vec<u8> {
     img.into_bytes()
 }
 
+fn get_canvas_context(canvas_id: &str) -> CanvasRenderingContext2d {
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+    let canvas: HtmlCanvasElement = document
+        .get_element_by_id(canvas_id)
+        .unwrap()
+        .dyn_into()
+        .unwrap();
+    canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into()
+        .unwrap()
+}
+
 #[wasm_bindgen]
 pub fn draw_chr(ines: Vec<u8>, canvas_id: &str) {
     let ines = INesFile::new(ines).unwrap();
     let img = read_chr(&ines);
     let image_data = ImageData::new_with_u8_clamped_array(Clamped(&img), 128).unwrap();
 
-    let canvas = window()
-        .unwrap()
-        .document()
-        .unwrap()
-        .get_element_by_id(canvas_id)
-        .unwrap();
-    let canvas: HtmlCanvasElement = canvas.dyn_into().unwrap();
-    let ctx: CanvasRenderingContext2d = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into()
-        .unwrap();
+    let ctx = get_canvas_context(canvas_id);
     ctx.put_image_data(&image_data, 0.0, 0.0).unwrap();
 }
 
