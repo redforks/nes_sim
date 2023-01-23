a34d296764f280254575e5dd1d36aaf8c176a0a3
forks <redforks@gmail.com>
2023-01-23 14:42:13 +0800

    Mapper0

--- nes_core/src/nes/mapper/mapper0.rs
+++ nes_core/src/nes/mapper/mapper0.rs
@@ -1,4 +1,4 @@
-use super::CARTRIRIDGE_START_ADDR;
+use super::CARTRIDGE_START_ADDR;
 use crate::mcu::Mcu;
 use crate::nes::mapper::Cartridge;
 
@@ -6,6 +6,18 @@ pub struct Mapper0 {
     prg_rom: [u8; 0x8000],
     chr_rom: [u8; 0x2000],
     ram: [u8; 0x4000 - 0x20],
+}
+
+impl Mapper0 {
+    pub fn new(prg_rom: &[u8], chr_rom: &[u8]) -> Self {
+        debug_assert!(prg_rom.len() <= 0x8000);
+        debug_assert!(chr_rom.len() <= 0x2000);
+
+        let mut r = Self::default();
+        r.prg_rom[0..prg_rom.len()].copy_from_slice(prg_rom);
+        r.chr_rom[0..chr_rom.len()].copy_from_slice(chr_rom);
+        r
+    }
 }
 
 impl Default for Mapper0 {
@@ -21,9 +33,7 @@ impl Default for Mapper0 {
 impl Mcu for Mapper0 {
     fn read(&self, address: u16) -> u8 {
         match address {
-            CARTRIRIDGE_START_ADDR..=0x7fff => {
-                self.ram[(address - CARTRIRIDGE_START_ADDR) as usize]
-            }
+            CARTRIDGE_START_ADDR..=0x7fff => self.ram[(address - CARTRIDGE_START_ADDR) as usize],
             0x8000..=0xffff => self.prg_rom[(address - 0x8000) as usize],
             _ => unreachable!(),
         }
@@ -31,8 +41,8 @@ impl Mcu for Mapper0 {
 
     fn write(&mut self, address: u16, value: u8) {
         match address {
-            CARTRIRIDGE_START_ADDR..=0x7fff => {
-                self.ram[(address - CARTRIRIDGE_START_ADDR) as usize] = value
+            CARTRIDGE_START_ADDR..=0x7fff => {
+                self.ram[(address - CARTRIDGE_START_ADDR) as usize] = value
             }
             0x8000..=0xffff => {
                 // ignore write to rom
@@ -44,22 +54,22 @@ impl Mcu for Mapper0 {
 
 impl Cartridge for Mapper0 {
     fn pattern_ref(&self) -> &[u8] {
-        todo!()
+        &self.chr_rom
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::nes::mapper::CARTRIRIDGE_START_ADDR;
+    use crate::nes::mapper::CARTRIDGE_START_ADDR;
 
     #[test]
     fn mcu() {
         let mut mcu = Mapper0::default();
 
         // read-write ram
-        mcu.write(CARTRIRIDGE_START_ADDR, 0x01);
-        assert_eq!(mcu.read(CARTRIRIDGE_START_ADDR), 0x01);
+        mcu.write(CARTRIDGE_START_ADDR, 0x01);
+        assert_eq!(mcu.read(CARTRIDGE_START_ADDR), 0x01);
         assert_eq!(mcu.ram[0], 0x01);
         mcu.write(0x7fff, 0x03);
         assert_eq!(mcu.read(0x7fff), 0x03);
@@ -70,5 +80,12 @@ mod tests {
         // ignore write to rom
         mcu.write(0x8000, 0x03);
         assert_eq!(mcu.prg_rom[0], 0x02);
+    }
+
+    #[test]
+    fn cartridge() {
+        let mcu = Mapper0::new(&[0; 0], &[1, 2]);
+        assert_eq!(1, mcu.pattern_ref()[0]);
+        assert_eq!(2, mcu.pattern_ref()[1]);
     }
 }
