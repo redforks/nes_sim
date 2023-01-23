d7555c53a36188927d7e21e51c8ca9e1f19340b8
forks <redforks@gmail.com>
2023-01-23 11:42:21 +0800

    Mapper0

--- nes_core/src/nes/mapper.rs
+++ nes_core/src/nes/mapper.rs
@@ -1,12 +1,14 @@
 use crate::ines::INesFile;
 use crate::mcu::Mcu;
 
+const CARTRIRIDGE_START_ADDR: u16 = 0x4020;
+
 mod mapper0;
 mod mmc1;
 
 pub fn create_cartridge(f: &INesFile) -> Box<dyn Cartridge> {
     match f.header().mapper_no {
-        0 => Box::new(mapper0::Mapper0 {}),
+        0 => Box::new(mapper0::Mapper0::default()),
         1 => Box::new(mmc1::MMC1::new(f.read_prg_rom())),
         _ => panic!("Unsupported cartridge mapper no: {}", f.header().mapper_no),
     }
