edcda2f9299ed2820e6cfd20138190dac757bf02
forks <redforks@gmail.com>
2023-01-23 11:42:21 +0800

    Mapper0

--- nes_core/src/nes/mapper/mapper0.rs
+++ nes_core/src/nes/mapper/mapper0.rs
@@ -1,17 +1,44 @@
+use super::CARTRIRIDGE_START_ADDR;
 use crate::mcu::Mcu;
 use crate::nes::mapper::Cartridge;
 
 pub struct Mapper0 {
-    // todo
+    prg_rom: [u8; 0x8000],
+    chr_rom: [u8; 0x2000],
+    ram: [u8; 0x4000 - 0x20],
+}
+
+impl Default for Mapper0 {
+    fn default() -> Self {
+        Self {
+            prg_rom: [0; 0x8000],
+            chr_rom: [0; 0x2000],
+            ram: [0; 0x4000 - 0x20],
+        }
+    }
 }
 
 impl Mcu for Mapper0 {
     fn read(&self, address: u16) -> u8 {
-        todo!()
+        match address {
+            CARTRIRIDGE_START_ADDR..=0x7fff => {
+                self.ram[(address - CARTRIRIDGE_START_ADDR) as usize]
+            }
+            0x8000..=0xffff => self.prg_rom[(address - 0x8000) as usize],
+            _ => unreachable!(),
+        }
     }
 
     fn write(&mut self, address: u16, value: u8) {
-        todo!()
+        match address {
+            CARTRIRIDGE_START_ADDR..=0x7fff => {
+                self.ram[(address - CARTRIRIDGE_START_ADDR) as usize] = value
+            }
+            0x8000..=0xffff => {
+                // ignore write to rom
+            }
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -22,5 +49,30 @@ impl Cartridge for Mapper0 {
 
     fn pattern_mut(&mut self) -> &mut [u8] {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::nes::mapper::CARTRIRIDGE_START_ADDR;
+
+    #[test]
+    fn mcu() {
+        let mut mcu = Mapper0::default();
+
+        // read-write ram
+        mcu.write(CARTRIRIDGE_START_ADDR, 0x01);
+        assert_eq!(mcu.read(CARTRIRIDGE_START_ADDR), 0x01);
+        assert_eq!(mcu.ram[0], 0x01);
+        mcu.write(0x7fff, 0x03);
+        assert_eq!(mcu.read(0x7fff), 0x03);
+
+        mcu.prg_rom[0] = 0x02;
+        // read-write rom
+        assert_eq!(mcu.read(0x8000), 0x02);
+        // ignore write to rom
+        mcu.write(0x8000, 0x03);
+        assert_eq!(mcu.prg_rom[0], 0x02);
     }
 }
